1f51c2daa7da7c50cd6cbdc5f6d57b9d652f3999
meaksu <meaksu@havelsan.com.tr>
2025-07-28 15:15:29 +0300

    library usage added

--- src/main/java/tr/com/altyapi/staj/DemoService/controller/DemoController.java
+++ src/main/java/tr/com/altyapi/staj/DemoService/controller/DemoController.java
@@ -2,13 +2,15 @@ package tr.com.altyapi.staj.DemoService.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tr.com.altyapi.staj.DemoApi;
 
 @RestController
 public class DemoController {
 
     @RequestMapping("/hello")
-    public String hello(){
-        return "Demo Service Hello";
+    public String hello() {
+        DemoApi demoApi = new DemoApi();
+        return "Demo Service Hello : " + demoApi.getHelloString();
     }
 
 }
