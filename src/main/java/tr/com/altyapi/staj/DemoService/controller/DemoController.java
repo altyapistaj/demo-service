59e0e49b3cdf72c9264ff2b6f6df299cbd071c82
meaksu <meaksu@havelsan.com.tr>
2025-07-29 07:48:20 +0300

    Revert "library usage added"
    This reverts commit 1f51c2daa7da7c50cd6cbdc5f6d57b9d652f3999.

--- src/main/java/tr/com/altyapi/staj/DemoService/controller/DemoController.java
+++ src/main/java/tr/com/altyapi/staj/DemoService/controller/DemoController.java
@@ -2,15 +2,13 @@ package tr.com.altyapi.staj.DemoService.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import tr.com.altyapi.staj.DemoApi;
 
 @RestController
 public class DemoController {
 
     @RequestMapping("/hello")
-    public String hello() {
-        DemoApi demoApi = new DemoApi();
-        return "Demo Service Hello : " + demoApi.getHelloString();
+    public String hello(){
+        return "Demo Service Hello";
     }
 
 }
